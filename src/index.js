582243bdce9fe47681a9a49ddf3dc2805ea7bf1a
Eirini Britzolaki <irenebritzolaki@gmail.com>
2024-02-08 14:13:42 +0200

    Test task and project modules with dummy content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { Project } from "./modules/project";
+import { Task } from "./modules/task";
+
 console.log("Okayyyyyyyyyyyyyy");
+
+const projectWork = new Project("Work");
+projectWork.addNewTask(
+  new Task("Work on odin-todo-list", "euvhsuhv", "13/02", "high", projectWork)
+);
+projectWork.addNewTask(
+  new Task("Read JS in real world section", "---", "", "low", projectWork)
+);
+console.log(projectWork);
