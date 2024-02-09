429e9d1c9ae2b22fa89e7593d61fc3a2dc486bef
Eirini Britzolaki <irenebritzolaki@gmail.com>
2024-02-09 15:33:11 +0200

    Update task and view task details style

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -16,26 +16,86 @@ export const displayController = (() => {
     }
   };
 
+  const createTaskDiv = (task) => {
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+
+    const basicDiv = document.createElement("div");
+    basicDiv.className = "basic";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function () {
+      task.toggleComplete();
+    });
+    if (task.completed) checkbox.checked = true;
+    basicDiv.appendChild(checkbox);
+
+    const label = document.createElement("label");
+    label.innerText = task.title;
+    basicDiv.appendChild(label);
+
+    const dueDate = document.createElement("p");
+    dueDate.className = "dueDate";
+    dueDate.innerText = task.dueDate;
+    basicDiv.appendChild(dueDate);
+
+    // const priority = document.createElement("p");
+    // priority.className = "priority";
+    // priority.innerText = task.priority;
+    // taskDiv.appendChild(priority);
+
+    const viewBtn = document.createElement("button");
+    viewBtn.className = "view-btn";
+    viewBtn.innerText = "View";
+    basicDiv.appendChild(viewBtn);
+
+    taskDiv.appendChild(basicDiv);
+
+    const detailsDiv = document.createElement("div");
+    detailsDiv.className = "details";
+    detailsDiv.style.display = "none";
+
+    const h4 = document.createElement("h4");
+    h4.innerText = "Description:";
+    detailsDiv.appendChild(h4);
+
+    const description = document.createElement("p");
+    description.className = "description";
+    description.innerText = task.description;
+    detailsDiv.appendChild(description);
+
+    taskDiv.appendChild(detailsDiv);
+
+    viewBtn.addEventListener("click", () => {
+      if (detailsDiv.style.display === "none")
+        detailsDiv.style.display = "initial";
+      else detailsDiv.style.display = "none";
+    });
+
+    return taskDiv;
+  };
+
   const displayTasksList = (project) => {
     const tasksListDiv = document.querySelector(".tasks-container");
     tasksListDiv.innerHTML = "";
 
     for (let task of project.tasks) {
-      const taskItem = document.createElement("task");
-      taskItem.className = "task";
+      // const taskItem = document.createElement("div");
+      // taskItem.className = "task";
 
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.addEventListener("change", function () {
-        task.toggleComplete();
-      });
-      if (task.completed) checkbox.checked = true;
-      taskItem.appendChild(checkbox);
+      // const checkbox = document.createElement("input");
+      // checkbox.type = "checkbox";
+      // checkbox.addEventListener("change", function () {
+      //   task.toggleComplete();
+      // });
+      // if (task.completed) checkbox.checked = true;
+      // taskItem.appendChild(checkbox);
 
-      const label = document.createElement("label");
-      label.innerText = task.title;
-      taskItem.appendChild(label);
-      tasksListDiv.appendChild(taskItem);
+      // const label = document.createElement("label");
+      // label.innerText = task.title;
+      // taskItem.appendChild(label);
+      tasksListDiv.appendChild(createTaskDiv(task));
     }
   };
 
